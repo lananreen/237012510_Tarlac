667fc9f3bc0e071aa61272f51f88e2760775f61e
root <root@Lana>
2024-07-12 17:59:27 +0800

    This batch file opens 5 websites, launches the calculator and notepad applications, and initiates a system shutdown

---
+++ BSCRIPT01.bat
@@ -0,0 +1,12 @@
+@echo off
+
+start "" "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+start "" "https://www.facebook.com/"
+start "" "https://www.instagram.com/"
+start "" "https://www.x.com/"
+start "" "https://www.messenger.com/"
+start calc
+start notepad
+
+pause
+shutdown -s
